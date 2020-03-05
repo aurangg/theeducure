ea4da511bce82f8444062825236bb47e035eb538
aurangg <aurangzebbbk@gmail.com>
2020-03-06 00:31:52 +0500

    Updates

---
+++ precache-manifest.f393eb913526df55c573c5d0e26ae4f9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "777b3e1e5f793aaa25af9eb56f1975d0",
+    "url": "/theeducure/index.html"
+  },
+  {
+    "revision": "e02a8e5a797d80c3236f",
+    "url": "/theeducure/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "15b32ff026574006bb80",
+    "url": "/theeducure/static/css/main.608e1557.chunk.css"
+  },
+  {
+    "revision": "e02a8e5a797d80c3236f",
+    "url": "/theeducure/static/js/2.a9a09ec2.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/theeducure/static/js/2.a9a09ec2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "15b32ff026574006bb80",
+    "url": "/theeducure/static/js/main.187e89cf.chunk.js"
+  },
+  {
+    "revision": "bee265db4adf29983518",
+    "url": "/theeducure/static/js/runtime-main.0b14a2c1.js"
+  }
+]);
